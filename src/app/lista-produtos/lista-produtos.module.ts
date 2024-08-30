aa551ebc5d4993ad57e9b71ce0b17129cd817b99
emaildoissa@gmail.com <emaildoissa@gmail.com>
2024-08-30 15:46:03 -0300

    styles

--- src/app/lista-produtos/lista-produtos.module.ts
+++ src/app/lista-produtos/lista-produtos.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatGridListModule } from '@angular/material/grid-list';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatGridListModule
+    MatGridListModule,
+    MatToolbarModule
   ]
 })
 export class ListaProdutosModule { }
