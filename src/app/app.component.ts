d795fba30a7ed7a8655649305a1cf2fbb9c9e689
emaildoissa@gmail.com <emaildoissa@gmail.com>
2024-08-30 15:02:20 -0300

    dados ok.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatGridListModule } from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    CommonModule,
+    MatGridListModule,
+    MatCardModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'vendas';
+  produtos: any[] = [];
+
+  constructor() {
+
+    import(`../app/assets/dados.json`).then(data => {
+      this.produtos = data.default;
+
+    });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'lista-produtos'},
+
+{ path: 'lista-produtos',
+ loadChildren: () => import('./lista-produtos/lista-produtos.module').then(m => m.ListaProdutosModule)}
+
+];
